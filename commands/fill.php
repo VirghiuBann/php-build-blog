11ecfd36491516af72c24a44e52de6f46222f98f
virghiu@Virjhiu <virghiu@gmail.com>
2020-06-18 00:09:49 -0400

    add pagination post

--- commands/fill.php
+++ commands/fill.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $faker = Faker\Factory::create();
 
 $pdo = new PDO('mysql:dbname=blog;host=mysql', 'laradock', 'laradock', [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 ]);
 
 $pdo->exec('SET FOREIGN_KEY_CHECKS = 0');
@@ -17,19 +17,19 @@ $pdo->exec('SET FOREIGN_KEY_CHECKS = 1');
 $posts = [];
 $categories = [];
 
-for($i=0; $i <20; $i++) {
-  $pdo->exec("INSERT INTO post SET name='{$faker->sentence()}', slug='{$faker->slug}', created_at='{$faker->date} {$faker->time}', content='{$faker->paragraphs(rand(3,15), true)}' ");
+for ($i = 0; $i < 50; $i++) {
+  $pdo->exec("INSERT INTO post SET name='{$faker->sentence()}', slug='{$faker->slug}', created_at='{$faker->date} {$faker->time}', content='{$faker->paragraphs(rand(3, 15), true)}' ");
   $posts[] = $pdo->lastInsertId();
 }
 
-for($i=0; $i <20; $i++) {
+for ($i = 0; $i < 20; $i++) {
   $pdo->exec("INSERT INTO category SET name='{$faker->sentence(3)}', slug='{$faker->slug}'");
   $categories[] = $pdo->lastInsertId();
 }
 
-foreach($posts as $post) {
+foreach ($posts as $post) {
   $randomCategories = $faker->randomElements($categories, rand(0, count($categories)));
-    foreach($randomCategories as $category) {
+  foreach ($randomCategories as $category) {
     $pdo->exec("INSERT INTO post_category SET post_id=$post, category_id=$category");
   }
 }
